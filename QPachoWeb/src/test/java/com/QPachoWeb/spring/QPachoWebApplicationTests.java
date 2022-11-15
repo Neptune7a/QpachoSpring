8d09c49b3ccc8479b6c56c1bdc254a1e7bcd8b32
Jesus Andrey Albarracín Córdiba <jesusandreyalco@ufps.edu.co>
2022-11-15 14:16:25 -0500

    Primer commit, creacion del repositorio

---
+++ QPachoWeb/src/test/java/com/QPachoWeb/spring/QPachoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.QPachoWeb.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QPachoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
