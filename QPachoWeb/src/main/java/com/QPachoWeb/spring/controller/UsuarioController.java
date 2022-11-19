a15e3f066d29fa550c7b74430c9d216368f36ed6
jesus <jesus@DESKTOP-V0HG2M3>
2022-11-19 11:32:14 -0500

    Correccion de errores

--- QPachoWeb/src/main/java/com/QPachoWeb/spring/controller/UsuarioController.java
+++ QPachoWeb/src/main/java/com/QPachoWeb/spring/controller/UsuarioController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.QPachoWeb.spring.entity.Noticia;
@@ -14,6 +15,7 @@ import com.QPachoWeb.spring.repository.UsuarioRepository;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
 	@Autowired
 	UsuarioRepository usuarioRepository;
